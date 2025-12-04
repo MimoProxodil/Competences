f3e09b8e136f23a8950b9a241516314757557f59
MimoProxodil <andreykotor@gmail.com>
2025-12-04 17:10:04 +0400

    Добавлены табы для уровня образования

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -3,10 +3,11 @@ import axios from "axios";
 import { useEffect, useState } from "react"
 import NavBar from "./NavBar";
 import CompetenceTypeDashboard from "../../features/competenceTypes/dashboard/CompetenceTypeDashboard";
+import EduLevelTabSwitcher from "./EduLevelTabSwitcher";
 
 function App() {
   const [competenceTypes, setCompetenceTypes] = useState<CompetenceType[]>([]);
-
+  
   useEffect(() =>{
     axios.get<CompetenceType[]>('http://localhost:5270/api/competencetypes')
     .then(response => setCompetenceTypes(response.data))
@@ -17,6 +18,7 @@ function App() {
     <Box sx={{bgcolor:'#eeeeee'}}>
       <CssBaseline />
       <NavBar />
+      <EduLevelTabSwitcher />
       <Container maxWidth='xl' sx={{mt:3}}>
         <CompetenceTypeDashboard competenceTypes ={competenceTypes} />
       </Container>
